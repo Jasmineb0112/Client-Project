fd3a79ab539296c4f806c262a023ec3a50b29dbf
BARAJAS JASMINE <553250@cart.ads>
2019-05-07 15:15:40 -0700

    html for home page

--- index.php
+++ index.php
@@ -5,6 +5,58 @@
     <title>Document</title>
 </head>
 <body>
+
+ <div class="nav bar">
+           <ul>
+            <li><a href="">Home</a></li>
+           <li><a href="">Portfolio</a></li>
+           <li><a href="">Detail</a></li>
+           <li><a href="">Contact</a></li>
+           </ul>
+    </div>
+<!-- goes at the top of the page-->
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quis excepturi dolore explicabo et aspernatur provident voluptatum in ex reiciendis dolorum fugiat optio cumque laudantium, sunt aliquid itaque accusantium suscipit fugit.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae sit illo nulla iusto perspiciatis, et, iure ea labore harum ducimus velit, esse quibusdam sed, ullam vitae facere alias pariatur nihil.Lorem ipsum dolor sit amet, consectetur adipisicing elit. In quasi consectetur ea temporibus obcaecati voluptatibus. Velit, rerum, cumque ipsam non sunt repudiandae! Quo sapiente provident explicabo beatae maxime, error corporis.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor voluptate autem deserunt perspiciatis. Ab eligendi maiores nobis sit consectetur facere nesciunt eos, distinctio veniam quaerat fuga, impedit voluptatem praesentium illo.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laborum id odit, voluptatum ipsum adipisci tempore soluta voluptatibus obcaecati pariatur iste tenetur a esse. Provident quia, nisi porro sequi ipsa eum!</p>
+ <button>Button</button>
+
+ <img src="http://placehold.it/500x500" alt="replace me">
+
+<!-- parallax goes here -->
+ <img src="http://placehold.it/1890x400" alt="replace me">
+
+<!-- steps to file -->
+<!-- the images are going to be icons-->
+<!--buttons , MAYBE-->
+ <img src="http://placehold.it/150x150" alt="replace me">
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae quisquam ipsa, iste ea nam aperiam culpa incidunt suscipit repellendus accusamus fugiat quidem fuga, est nulla, modi ad omnis repudiandae dolores?Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid consequatur delectus omnis quas laudantium perferendis totam quae, saepe architecto alias facilis dicta, eaque qui nulla aut veniam non dolor suscipit.</p>
+<button>Hello</button> 
+
+<img src="http://placehold.it/150x150" alt="replace me">
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae quisquam ipsa, iste ea nam aperiam culpa incidunt suscipit repellendus accusamus fugiat quidem fuga, est nulla, modi ad omnis repudiandae dolores?Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid consequatur delectus omnis quas laudantium perferendis totam quae, saepe architecto alias facilis dicta, eaque qui nulla aut veniam non dolor suscipit.</p>
+<button>Hello</button> 
+
+<img src="http://placehold.it/150x150" alt="replace me">
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae quisquam ipsa, iste ea nam aperiam culpa incidunt suscipit repellendus accusamus fugiat quidem fuga, est nulla, modi ad omnis repudiandae dolores?Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid consequatur delectus omnis quas laudantium perferendis totam quae, saepe architecto alias facilis dicta, eaque qui nulla aut veniam non dolor suscipit.</p>
+<button>Hello</button>
+
+<!-- mission statement -->
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur esse eius ab maxime corporis, laborum voluptatem, id quasi ipsa blanditiis enim quam pariatur quos voluptates fugiat qui assumenda hic quod.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt, earum ad sapiente, expedita velit, architecto magnam minima reprehenderit, modi vero dolore. Delectus velit nulla ea magni ducimus perferendis suscipit amet!Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veritatis consequatur, voluptas id, perferendis mollitia dignissimos unde iure laboriosam, molestiae repellat omnis, fugiat asperiores quae rerum! Quas inventore consectetur incidunt neque.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis nisi, nemo optio corporis dolorem, amet sint. Explicabo natus, doloremque voluptatibus neque, laudantium unde. Nulla nostrum maiores minima delectus, ea mollitia.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Labore quasi quisquam repellendus dignissimos atque mollitia similique ducimus vitae, obcaecati placeat ad at saepe, id! Cumque voluptates, tenetur esse commodi voluptatum.</p>
+
+<!-- customer reviews -->
+<!-- the images on the bottom of the images are the stars for the reviews-->
+
+ <img src="http://placehold.it/200x200" alt="replace me">
+  <img src="http://placehold.it/150x50" alt="replace me">
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae quisquam ipsa, iste ea nam aperiam culpa incidunt suscipit repellendus accusamus fugiat quidem fuga, est nulla, modi ad omnis repudiandae dolores?Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid consequatur delectus omnis quas laudantium perferendis totam quae, saepe architecto alias facilis dicta, eaque qui nulla aut veniam non dolor suscipit.</p>
+
+ <img src="http://placehold.it/200x200" alt="replace me">
+   <img src="http://placehold.it/150x50" alt="replace me">
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae quisquam ipsa, iste ea nam aperiam culpa incidunt suscipit repellendus accusamus fugiat quidem fuga, est nulla, modi ad omnis repudiandae dolores?Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid consequatur delectus omnis quas laudantium perferendis totam quae, saepe architecto alias facilis dicta, eaque qui nulla aut veniam non dolor suscipit.</p>
     
+     <img src="http://placehold.it/200x200" alt="replace me">
+       <img src="http://placehold.it/150x50" alt="replace me">
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae quisquam ipsa, iste ea nam aperiam culpa incidunt suscipit repellendus accusamus fugiat quidem fuga, est nulla, modi ad omnis repudiandae dolores?Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid consequatur delectus omnis quas laudantium perferendis totam quae, saepe architecto alias facilis dicta, eaque qui nulla aut veniam non dolor suscipit.</p>
+<!--  -->
+
+
 </body>
 </html>
