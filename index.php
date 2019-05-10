97ad74aac8635c76e92facabbf52e8ca834a3420
BARAJAS JASMINE <553250@cart.ads>
2019-05-10 14:47:09 -0700

    added more html to the pages, added the comments from google, and added more css to the page

--- index.php
+++ index.php
@@ -18,12 +18,15 @@
     </div>
 <!-- goes at the top of the page-->
 
-<div class="forty">
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quis excepturi dolore explicabo et aspernatur provident voluptatum in ex reiciendis dolorum fugiat optio cumque laudantium, sunt aliquid itaque accusantium suscipit fugit.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae sit illo nulla iusto perspiciatis, et, iure ea labore harum ducimus velit, esse quibusdam sed, ullam vitae facere alias pariatur nihil.Lorem ipsum dolor sit amet, consectetur adipisicing elit. In quasi consectetur ea temporibus obcaecati voluptatibus. Velit, rerum, cumque ipsam non sunt repudiandae! Quo sapiente provident explicabo beatae maxime, error corporis.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor voluptate autem deserunt perspiciatis. Ab eligendi maiores nobis sit consectetur facere nesciunt eos, distinctio veniam quaerat fuga, impedit voluptatem praesentium illo.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laborum id odit, voluptatum ipsum adipisci tempore soluta voluptatibus obcaecati pariatur iste tenetur a esse. Provident quia, nisi porro sequi ipsa eum!</p>
-     <button>Button</button>
-</div>
-
- <img src="http://placehold.it/500x500" alt="replace me">
+<div class="flex">
+   
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quis excepturi dolore explicabo et aspernatur provident voluptatum in ex reiciendis dolorum fugiat optio cumque laudantium, sunt aliquid itaque accusantium suscipit fugit.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae sit illo nulla iusto perspiciatis, et, iure ea labore harum ducimus velit, esse quibusdam sed, ullam vitae facere alias pariatur nihil.Lorem ipsum dolor sit amet, consectetur adipisicing elit. In quasi consectetur ea temporibus obcaecati voluptatibus. Velit, rerum, cumque ipsam non sunt repudiandae! Quo sapiente provident explicabo beatae maxime, error corporis.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor voluptate autem deserunt perspiciatis. Ab eligendi maiores nobis sit consectetur facere nesciunt eos, distinctio veniam quaerat fuga, impedit voluptatem praesentium illo.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laborum id odit, voluptatum ipsum adipisci tempore soluta voluptatibus obcaecati pariatur iste tenetur a esse. Provident quia, nisi porro sequi ipsa eum!</p>
+         <button>Button</button><br>
+    
+    
+    
+        <img src="http://placehold.it/400x400" alt="replace me">
+         </div>
 
 <!-- parallax goes here -->
  <img src="http://placehold.it/1890x400" alt="replace me">
@@ -31,8 +34,13 @@
 <!-- steps to file -->
 <!-- the images are going to be icons-->
 <!--buttons , MAYBE-->
- <img src="http://placehold.it/150x150" alt="replace me">
-<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae quisquam ipsa, iste ea nam aperiam culpa incidunt suscipit repellendus accusamus fugiat quidem fuga, est nulla, modi ad omnis repudiandae dolores?Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid consequatur delectus omnis quas laudantium perferendis totam quae, saepe architecto alias facilis dicta, eaque qui nulla aut veniam non dolor suscipit.</p>
+
+<div class="thirds">
+    <img src="http://placehold.it/150x150" alt="replace me">
+     </div>
+    <div class="thirds">
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae quisquam ipsa, iste ea nam aperiam culpa incidunt suscipit repellendus accusamus fugiat quidem fuga, est nulla, modi ad omnis repudiandae dolores?Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid consequatur delectus omnis quas laudantium perferendis totam quae, saepe architecto alias facilis dicta, eaque qui nulla aut veniam non dolor suscipit.</p>
+     </div>
 <button>Hello</button> 
 
 <img src="http://placehold.it/150x150" alt="replace me">
@@ -53,38 +61,44 @@
 
  <img src="http://placehold.it/200x200" alt="replace me">
   <img src="http://placehold.it/150x50" alt="replace me">
-<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae quisquam ipsa, iste ea nam aperiam culpa incidunt suscipit repellendus accusamus fugiat quidem fuga, est nulla, modi ad omnis repudiandae dolores?Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid consequatur delectus omnis quas laudantium perferendis totam quae, saepe architecto alias facilis dicta, eaque qui nulla aut veniam non dolor suscipit.</p>
+<p>I highly recommend A&M for all of your tax service needs.  Not only did they provide excellent customer service, but they also made sure not to miss a single thing with my return.  Their prices are very reasonable, they're very quick, and they take the time to get to build personal relationships with you.  Thank you A&M!!</p>
 
  <img src="http://placehold.it/200x200" alt="replace me">
    <img src="http://placehold.it/150x50" alt="replace me">
-<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae quisquam ipsa, iste ea nam aperiam culpa incidunt suscipit repellendus accusamus fugiat quidem fuga, est nulla, modi ad omnis repudiandae dolores?Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid consequatur delectus omnis quas laudantium perferendis totam quae, saepe architecto alias facilis dicta, eaque qui nulla aut veniam non dolor suscipit.</p>
+<p>I have used larger tax companies until this year when I tried A&M. I cannot say enough about how customer sensitive they are. The customer service was amazing and it cost much less than I was paying before. They did not miss a thing and filing was done in a fraction of the time. Highly recommended. These guys are legit and I will be returning next year. Thank you guys for the great experience.</p>
     
      <img src="http://placehold.it/200x200" alt="replace me">
        <img src="http://placehold.it/150x50" alt="replace me">
-<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae quisquam ipsa, iste ea nam aperiam culpa incidunt suscipit repellendus accusamus fugiat quidem fuga, est nulla, modi ad omnis repudiandae dolores?Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid consequatur delectus omnis quas laudantium perferendis totam quae, saepe architecto alias facilis dicta, eaque qui nulla aut veniam non dolor suscipit.</p>
+<p>A&M provided amazing customer service while providing specific details of the process. This place is time efficient and affordable! I would definitely recommend to others. Staff is great and personable!
+<p>
 
 <img src="http://placehold.it/200x200" alt="replace me">
   <img src="http://placehold.it/150x50" alt="replace me">
-<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae quisquam ipsa, iste ea nam aperiam culpa incidunt suscipit repellendus accusamus fugiat quidem fuga, est nulla, modi ad omnis repudiandae dolores?Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid consequatur delectus omnis quas laudantium perferendis totam quae, saepe architecto alias facilis dicta, eaque qui nulla aut veniam non dolor suscipit.</p>
+<p>Very happy with their service. Took the time to look over everything to make sure I was getting the best refund. This was the first year I ever got back a huge refund. I also like how they explained the new laws and how they will effect me from now on. 
+
+Sounds funny, but they even change the temp in the room to make sure I was comfortable! 
+
+ I highly recommend them</p>
 
  <img src="http://placehold.it/200x200" alt="replace me">
    <img src="http://placehold.it/150x50" alt="replace me">
-<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae quisquam ipsa, iste ea nam aperiam culpa incidunt suscipit repellendus accusamus fugiat quidem fuga, est nulla, modi ad omnis repudiandae dolores?Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid consequatur delectus omnis quas laudantium perferendis totam quae, saepe architecto alias facilis dicta, eaque qui nulla aut veniam non dolor suscipit.</p>
+<p>I am amazed on how quickly and accurately A&M Tax Service prepared my taxes.  A line by line review was thoroughly and patiently explained.  With the ever changing tax laws, I trust their tax planning advice for the next year.  Reasonable price and excellent customer service warrants more than 5 stars.</p>
     
      <img src="http://placehold.it/200x200" alt="replace me">
        <img src="http://placehold.it/150x50" alt="replace me">
-<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae quisquam ipsa, iste ea nam aperiam culpa incidunt suscipit repellendus accusamus fugiat quidem fuga, est nulla, modi ad omnis repudiandae dolores?Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid consequatur delectus omnis quas laudantium perferendis totam quae, saepe architecto alias facilis dicta, eaque qui nulla aut veniam non dolor suscipit.</p>
+<p>John is extremely knowledgeable and helpful. He is always reliable and accommodating to his clients. He cuts out wasted time and paperwork that I’ve dealt with at other tax prep companies that overcharge for nice offices and “peace of mind”. He gets to the point and shows you your options. When I had audits from previous companies filings, he fixed the mess. Highly recommend</p>
 <img src="http://placehold.it/200x200" alt="replace me">
   <img src="http://placehold.it/150x50" alt="replace me">
-<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae quisquam ipsa, iste ea nam aperiam culpa incidunt suscipit repellendus accusamus fugiat quidem fuga, est nulla, modi ad omnis repudiandae dolores?Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid consequatur delectus omnis quas laudantium perferendis totam quae, saepe architecto alias facilis dicta, eaque qui nulla aut veniam non dolor suscipit.</p>
+<p>John is very professional and makes your tax experience easy. He's very detailed and takes time explaining everything. We will definitely be back next year!!</p>
 
  <img src="http://placehold.it/200x200" alt="replace me">
    <img src="http://placehold.it/150x50" alt="replace me">
-<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae quisquam ipsa, iste ea nam aperiam culpa incidunt suscipit repellendus accusamus fugiat quidem fuga, est nulla, modi ad omnis repudiandae dolores?Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid consequatur delectus omnis quas laudantium perferendis totam quae, saepe architecto alias facilis dicta, eaque qui nulla aut veniam non dolor suscipit.</p>
+<p>Very professional, timely, and efficient. I have been seeing John for taxes for two years now and it’s always been a seamless process. Highly recommend!</p>
     
      <img src="http://placehold.it/200x200" alt="replace me">
        <img src="http://placehold.it/150x50" alt="replace me">
-<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae quisquam ipsa, iste ea nam aperiam culpa incidunt suscipit repellendus accusamus fugiat quidem fuga, est nulla, modi ad omnis repudiandae dolores?Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid consequatur delectus omnis quas laudantium perferendis totam quae, saepe architecto alias facilis dicta, eaque qui nulla aut veniam non dolor suscipit.</p>
+<p>Fantastic service, great price! They get you what you are entitled to!
+</p>
 <!--  -->
 
 
--- team.php
+++ team.php
@@ -9,8 +9,13 @@
 <body>
     <!-- Image will be a cartoon of the TEAM-->
       <!-- the image is for the appoinment and the team page, the h1 is going on top of the image -->
-     <img src="http://placehold.it/1890x400" alt="replace me">
-<h1>MEET THE TEAM</h1>
+  <div class="container parallax">
+            
+           <div class="centered">
+                 <h1>MEET THE TEAM</h1>
+            </div>
+
+     </div>
 <!-- Pictures are cartoons of team-->
  <img src="http://placehold.it/500x500" alt="replace me">
 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam quae, ad, autem adipisci maxime cum asperiores. Saepe enim omnis atque facere blanditiis voluptate ea, quod possimus ex. Deserunt, eaque, cumque!Lorem ipsum dolor sit amet, consectetur adipisicing elit. Esse et accusantium quos iusto ea ad aspernatur a, fugiat totam consequuntur! Ducimus quas rerum eius, sapiente veritatis deserunt aliquam quisquam. Error.</p>
