db96f781367694e5f81bea4fd81b6edf1ec07f3f
BARAJAS JASMINE <553250@cart.ads>
2019-05-13 14:24:34 -0700

    tried to fix something

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 </head>
 <body>
 
- <div class="topnav">
+ <div class="top">
            <ul>
             <li><a href="index.php">Home</a></li>
            <li><a href="rates.php">Rates</a></li>
@@ -18,17 +18,21 @@
     </div>
 <!-- goes at the top of the page-->
 
-<div class="flex">
-   
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quis excepturi dolore explicabo et aspernatur provident voluptatum in ex reiciendis dolorum fugiat optio cumque laudantium, sunt aliquid itaque accusantium suscipit fugit.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae sit illo nulla iusto perspiciatis, et, iure ea labore harum ducimus velit, esse quibusdam sed, ullam vitae facere alias pariatur nihil.Lorem ipsum dolor sit amet, consectetur adipisicing elit. In quasi consectetur ea temporibus obcaecati voluptatibus. Velit, rerum, cumque ipsam non sunt repudiandae! Quo sapiente provident explicabo beatae maxime, error corporis.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor voluptate autem deserunt perspiciatis. Ab eligendi maiores nobis sit consectetur facere nesciunt eos, distinctio veniam quaerat fuga, impedit voluptatem praesentium illo.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laborum id odit, voluptatum ipsum adipisci tempore soluta voluptatibus obcaecati pariatur iste tenetur a esse. Provident quia, nisi porro sequi ipsa eum!</p>
-         <button>Button</button><br>
+<div class="flex-container">
+    <div class="flex">
+       
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quis excepturi dolore explicabo et aspernatur provident voluptatum in ex reiciendis dolorum fugiat optio cumque laudantium, sunt aliquid itaque accusantium suscipit fugit.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae sit illo nulla iusto perspiciatis, et, iure ea labore harum ducimus velit, esse quibusdam sed, ullam vitae facere alias pariatur nihil.Lorem ipsum dolor sit amet, consectetur adipisicing elit. In quasi consectetur ea temporibus obcaecati voluptatibus. Velit, rerum, cumque ipsam non sunt repudiandae! Quo sapiente provident explicabo beatae maxime, error corporis.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor voluptate autem deserunt perspiciatis. Ab eligendi maiores nobis sit consectetur facere nesciunt eos, distinctio veniam quaerat fuga, impedit voluptatem praesentium illo.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laborum id odit, voluptatum ipsum adipisci tempore soluta voluptatibus obcaecati pariatur iste tenetur a esse. Provident quia, nisi porro sequi ipsa eum!</p>
+             <button>Button</button><br>
     
-    
-    
-        <img src="http://placehold.it/400x400" alt="replace me">
-         </div>
-
-<!-- parallax goes here -->
+             </div>
+    <div class="flex">
+         <img src="http://placehold.it/400x400" alt="replace me">
+         
+         
+         
+         
+          </div>
+</div><!-- parallax goes here -->
  <img src="http://placehold.it/1890x400" alt="replace me">
 
 <!-- steps to file -->
@@ -53,18 +57,22 @@
 
 <!-- mission statement -->
 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur esse eius ab maxime corporis, laborum voluptatem, id quasi ipsa blanditiis enim quam pariatur quos voluptates fugiat qui assumenda hic quod.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt, earum ad sapiente, expedita velit, architecto magnam minima reprehenderit, modi vero dolore. Delectus velit nulla ea magni ducimus perferendis suscipit amet!Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veritatis consequatur, voluptas id, perferendis mollitia dignissimos unde iure laboriosam, molestiae repellat omnis, fugiat asperiores quae rerum! Quas inventore consectetur incidunt neque.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis nisi, nemo optio corporis dolorem, amet sint. Explicabo natus, doloremque voluptatibus neque, laudantium unde. Nulla nostrum maiores minima delectus, ea mollitia.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Labore quasi quisquam repellendus dignissimos atque mollitia similique ducimus vitae, obcaecati placeat ad at saepe, id! Cumque voluptates, tenetur esse commodi voluptatum.</p>
-
+<br>
 <!-- 9 customer reviews -->
 <!-- 9 customer reviews -->
 
 <!-- the images on the bottom of the images are the stars for the reviews-->
 
- <img src="http://placehold.it/200x200" alt="replace me">
-  <img src="http://placehold.it/150x50" alt="replace me">
-<p>I highly recommend A&M for all of your tax service needs.  Not only did they provide excellent customer service, but they also made sure not to miss a single thing with my return.  Their prices are very reasonable, they're very quick, and they take the time to get to build personal relationships with you.  Thank you A&M!!</p>
+<div class="thirds">
+     <img src="http://placehold.it/200x200" alt="replace me">
+      <img src="http://placehold.it/150x50" alt="replace me">
+     <p>I highly recommend A&M for all of your tax service needs.  Not only did they provide excellent customer service, but they also made sure not to miss a single thing with my return.  Their prices are very reasonable, they're very quick, and they take the time to get to build personal relationships with you.  Thank you A&M!!</p>
+ </div>
 
  <img src="http://placehold.it/200x200" alt="replace me">
-  <img src="imgs/stars.PNG" alt="">
+  <div class="stars">
+      <img src="imgs/stars.PNG" alt="">
+  </div>
    <img src="http://placehold.it/150x50" alt="replace me">
 <p>I have used larger tax companies until this year when I tried A&M. I cannot say enough about how customer sensitive they are. The customer service was amazing and it cost much less than I was paying before. They did not miss a thing and filing was done in a fraction of the time. Highly recommended. These guys are legit and I will be returning next year. Thank you guys for the great experience.</p>
     
