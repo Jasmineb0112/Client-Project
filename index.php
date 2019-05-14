ab98bcf1736413f12558cca2a85a42f0f637795d
BARAJAS JASMINE <553250@cart.ads>
2019-05-14 15:12:29 -0700

    added the images for the team page and I change the html and the css so it can look ok

--- index.php
+++ index.php
@@ -4,29 +4,34 @@
     <meta charset="UTF-8">
     <title>Home</title>
     <link rel="stylesheet" href="css/index.css">
+    <link href="https://fonts.googleapis.com/css?family=Fira+Sans|Raleway" rel="stylesheet">
 </head>
 <body>
 
  <div class="top">
+          <img class="float" src="imgs/logo-2.jpg" alt="" width="128" height="76">
            <ul>
             <li><a href="index.php">Home</a></li>
-           <li><a href="rates.php">Rates</a></li>
+              <li><a href="team.php">Team</a></li>
            <li><a href="appointment.php">Appointments</a></li>
-            <li><a href="team.php">Team</a></li>
            <li><a href="contact.php">Contact</a></li>
            </ul>
     </div>
 <!-- goes at the top of the page-->
 
 <div class="flex-container">
-    <div class="flex">
+    <div class="flex sixty">
        
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quis excepturi dolore explicabo et aspernatur provident voluptatum in ex reiciendis dolorum fugiat optio cumque laudantium, sunt aliquid itaque accusantium suscipit fugit.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae sit illo nulla iusto perspiciatis, et, iure ea labore harum ducimus velit, esse quibusdam sed, ullam vitae facere alias pariatur nihil.Lorem ipsum dolor sit amet, consectetur adipisicing elit. In quasi consectetur ea temporibus obcaecati voluptatibus. Velit, rerum, cumque ipsam non sunt repudiandae! Quo sapiente provident explicabo beatae maxime, error corporis.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor voluptate autem deserunt perspiciatis. Ab eligendi maiores nobis sit consectetur facere nesciunt eos, distinctio veniam quaerat fuga, impedit voluptatem praesentium illo.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laborum id odit, voluptatum ipsum adipisci tempore soluta voluptatibus obcaecati pariatur iste tenetur a esse. Provident quia, nisi porro sequi ipsa eum!</p>
-             <button>Button</button><br>
+            <p class="tax">	The best way to file your taxes in a great environment is with A&M Tax Service! Our representatives are dedicated to making your tax experience easy.
+</p>
+            <div class="right">
+                  <button>Find an Office</button><br>
+             </div>
     
              </div>
     <div class="flex">
-         <img src="http://placehold.it/400x400" alt="replace me">
+        <img src="imgs/home-temp.jpg" alt="">
+         <!--<img src="http://placehold.it/400x400" alt="replace me"> -->
          
          
          
@@ -39,75 +44,111 @@
 <!-- the images are going to be icons-->
 <!--buttons , MAYBE-->
 
-<div class="thirds">
-    <img src="http://placehold.it/150x150" alt="replace me">
-     </div>
-    <div class="thirds">
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae quisquam ipsa, iste ea nam aperiam culpa incidunt suscipit repellendus accusamus fugiat quidem fuga, est nulla, modi ad omnis repudiandae dolores?Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid consequatur delectus omnis quas laudantium perferendis totam quae, saepe architecto alias facilis dicta, eaque qui nulla aut veniam non dolor suscipit.</p>
-     </div>
-<button>Hello</button> 
 
-<img src="http://placehold.it/150x150" alt="replace me">
-<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae quisquam ipsa, iste ea nam aperiam culpa incidunt suscipit repellendus accusamus fugiat quidem fuga, est nulla, modi ad omnis repudiandae dolores?Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid consequatur delectus omnis quas laudantium perferendis totam quae, saepe architecto alias facilis dicta, eaque qui nulla aut veniam non dolor suscipit.</p>
-<button>Hello</button> 
-
-<img src="http://placehold.it/150x150" alt="replace me">
-<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae quisquam ipsa, iste ea nam aperiam culpa incidunt suscipit repellendus accusamus fugiat quidem fuga, est nulla, modi ad omnis repudiandae dolores?Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid consequatur delectus omnis quas laudantium perferendis totam quae, saepe architecto alias facilis dicta, eaque qui nulla aut veniam non dolor suscipit.</p>
-<button>Hello</button>
-
+   <div class="flex-container">
+        <div class="thirds">
+              <img src="http://placehold.it/150x150" alt="replace me">
+              <p class="left">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae quisquam ipsa, iste ea nam aperiam culpa incidunt suscipit repellendus accusamus fugiat quidem fuga, est nulla, modi ad omnis repudiandae dolores?</p>
+      
+          </div>
+          
+          
+       <div class="thirds">
+            <img src="http://placehold.it/150x150" alt="replace me">
+            <p class="left">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae quisquam ipsa, iste ea nam aperiam culpa incidunt suscipit repellendus accusamus fugiat quidem fuga, est nulla, modi ad omnis repudiandae dolores?</p>
+             </div>
+        
+       <div class="thirds">
+             <img src="http://placehold.it/150x150" alt="replace me">
+            <p class="left">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae quisquam ipsa, iste ea nam aperiam culpa incidunt suscipit repellendus accusamus fugiat quidem fuga, est nulla, modi ad omnis repudiandae dolores?</p>
+          
+        </div>
+        </div>
 <!-- mission statement -->
-<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur esse eius ab maxime corporis, laborum voluptatem, id quasi ipsa blanditiis enim quam pariatur quos voluptates fugiat qui assumenda hic quod.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt, earum ad sapiente, expedita velit, architecto magnam minima reprehenderit, modi vero dolore. Delectus velit nulla ea magni ducimus perferendis suscipit amet!Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veritatis consequatur, voluptas id, perferendis mollitia dignissimos unde iure laboriosam, molestiae repellat omnis, fugiat asperiores quae rerum! Quas inventore consectetur incidunt neque.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis nisi, nemo optio corporis dolorem, amet sint. Explicabo natus, doloremque voluptatibus neque, laudantium unde. Nulla nostrum maiores minima delectus, ea mollitia.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Labore quasi quisquam repellendus dignissimos atque mollitia similique ducimus vitae, obcaecati placeat ad at saepe, id! Cumque voluptates, tenetur esse commodi voluptatum.</p>
+<div class="flex-container">
+    <div class="box">
+        <p>Our firm’s mission is to develop personal relationships with our clients and a commitment to service. We provide many services to our customers to make sure we give them the best care. Each client receives their own personal attention and time. We are timely and responsive to any questions you may have. Our representatives provide you with expert advice that is not only accurate, but also dependable. We offer individual, business, and tax resolution services! We believe our firm’s rapid growth is largely due to our personalized customer service.
+        </p>
+    </div>
+</div>
 <br>
 <!-- 9 customer reviews -->
 <!-- 9 customer reviews -->
 
 <!-- the images on the bottom of the images are the stars for the reviews-->
 
-
-     <img src="http://placehold.it/200x200" alt="replace me">
-      <img src="http://placehold.it/150x50" alt="replace me">
-     <div class="thirds">
-         <p>I highly recommend A&M for all of your tax service needs.  Not only did they provide excellent customer service, but they also made sure not to miss a single thing with my return.  Their prices are very reasonable, they're very quick, and they take the time to get to build personal relationships with you.  Thank you A&M!!</p>
+<div class="flex-container">
+         <div class="three">
+             <img src="http://placehold.it/200x200" alt="replace me">
+                 <p class="left">I highly recommend A&M for all of your tax service needs.  Not only did they provide excellent customer service, but they also made sure not to miss a single thing with my return.  Their prices are very reasonable, they're very quick, and they take the time to get to build personal relationships with you.  Thank you A&M!!</p>
+                <img src="http://placehold.it/150x50" alt="replace me">
+         </div>
+            
+            <div class="three">
+               <img src="http://placehold.it/200x200" alt="replace me">
+                 <p class="left">A&M provided amazing customer service while providing specific details of the process. This place is time efficient and affordable! I would definitely recommend to others. Staff is great and personable!
+                 <p>
+                  <img src="http://placehold.it/150x50" alt="replace me">
+         </div>
+    <div class="three">
+          <img src="http://placehold.it/200x200" alt="replace me">
+             <p class="left">I have used larger tax companies until this year when I tried A&M. I cannot say enough about how customer sensitive they are. The customer service was amazing and it cost much less than I was paying before. They did not miss a thing and filing was done in a fraction of the time. Highly recommended. These guys are legit and I will be returning next year. Thank you guys for the great experience.</p>
+             <img src="http://placehold.it/150x50" alt="replace me">
      </div>
-
-
- <img src="http://placehold.it/200x200" alt="replace me">
- 
-   <img src="http://placehold.it/150x50" alt="replace me">
-<p>I have used larger tax companies until this year when I tried A&M. I cannot say enough about how customer sensitive they are. The customer service was amazing and it cost much less than I was paying before. They did not miss a thing and filing was done in a fraction of the time. Highly recommended. These guys are legit and I will be returning next year. Thank you guys for the great experience.</p>
+    </div>
     
-     <img src="http://placehold.it/200x200" alt="replace me">
-       <img src="http://placehold.it/150x50" alt="replace me">
-<p>A&M provided amazing customer service while providing specific details of the process. This place is time efficient and affordable! I would definitely recommend to others. Staff is great and personable!
-<p>
-
-<img src="http://placehold.it/200x200" alt="replace me">
-  <img src="http://placehold.it/150x50" alt="replace me">
-<p>Very happy with their service. Took the time to look over everything to make sure I was getting the best refund. This was the first year I ever got back a huge refund. I also like how they explained the new laws and how they will effect me from now on. 
-
-Sounds funny, but they even change the temp in the room to make sure I was comfortable! 
-
- I highly recommend them</p>
-
- <img src="http://placehold.it/200x200" alt="replace me">
-   <img src="http://placehold.it/150x50" alt="replace me">
-<p>I am amazed on how quickly and accurately A&M Tax Service prepared my taxes.  A line by line review was thoroughly and patiently explained.  With the ever changing tax laws, I trust their tax planning advice for the next year.  Reasonable price and excellent customer service warrants more than 5 stars.</p>
     
-     <img src="http://placehold.it/200x200" alt="replace me">
-       <img src="http://placehold.it/150x50" alt="replace me">
-<p>John is extremely knowledgeable and helpful. He is always reliable and accommodating to his clients. He cuts out wasted time and paperwork that I’ve dealt with at other tax prep companies that overcharge for nice offices and “peace of mind”. He gets to the point and shows you your options. When I had audits from previous companies filings, he fixed the mess. Highly recommend</p>
-<img src="http://placehold.it/200x200" alt="replace me">
-  <img src="http://placehold.it/150x50" alt="replace me">
-<p>John is very professional and makes your tax experience easy. He's very detailed and takes time explaining everything. We will definitely be back next year!!</p>
-
- <img src="http://placehold.it/200x200" alt="replace me">
-   <img src="http://placehold.it/150x50" alt="replace me">
-<p>Very professional, timely, and efficient. I have been seeing John for taxes for two years now and it’s always been a seamless process. Highly recommend!</p>
+<div class="flex-container">
+   <div class="three">
+       <img src="http://placehold.it/200x200" alt="replace me">
+        <p>Very happy with their service. Took the time to look over everything to make sure I was getting the best refund. This was the first year I ever got back a huge refund. I also like how they explained the new laws and how they will effect me from now on. 
+        
+        Sounds funny, but they even change the temp in the room to make sure I was comfortable! 
+        
+         I highly recommend them</p>
+        <img src="http://placehold.it/150x50" alt="replace me">
+   </div>
     
-     <img src="http://placehold.it/200x200" alt="replace me">
-       <img src="http://placehold.it/150x50" alt="replace me">
-<p>Fantastic service, great price! They get you what you are entitled to!
-</p>
+    
+    
+    <div class="three">
+          <img src="http://placehold.it/200x200" alt="replace me">
+             <p>I am amazed on how quickly and accurately A&M Tax Service prepared my taxes.  A line by line review was thoroughly and patiently explained.  With the ever changing tax laws, I trust their tax planning advice for the next year.  Reasonable price and excellent customer service warrants more than 5 stars.</p>
+           <img src="http://placehold.it/150x50" alt="replace me">
+     </div>
+    
+       
+        
+         <div class="three">
+             <img src="http://placehold.it/200x200" alt="replace me">
+                 <p>John is extremely knowledgeable and helpful. He is always reliable and accommodating to his clients. He cuts out wasted time and paperwork that I’ve dealt with at other tax prep companies that overcharge for nice offices and “peace of mind”. He gets to the point and shows you your options. When I had audits from previous companies filings, he fixed the mess. Highly recommend</p>
+               <img src="http://placehold.it/150x50" alt="replace me">
+         </div>
+    </div>
+
+<div class="flex-container">
+   <div class="three">
+         <img src="http://placehold.it/200x200" alt="replace me">
+        <p>John is very professional and makes your tax experience easy. He's very detailed and takes time explaining everything. We will definitely be back next year!!</p>
+          <img src="http://placehold.it/150x50" alt="replace me">
+    </div>
+    
+    
+    <div class="three">
+          <img src="http://placehold.it/200x200" alt="replace me">
+             <p>Very professional, timely, and efficient. I have been seeing John for taxes for two years now and it’s always been a seamless process. Highly recommend!</p>
+               <img src="http://placehold.it/150x50" alt="replace me">
+     </div>
+    
+        
+         <div class="three">
+             <img src="http://placehold.it/200x200" alt="replace me">
+                 <p>Fantastic service, great price! They get you what you are entitled to!
+                 </p>
+               <img src="http://placehold.it/150x50" alt="replace me">
+         </div>
+</div>
+
 <!--  -->
 
 
