64adcf0ff0c91cca89d2c3e8a625f57879cc5c69
BARAJAS JASMINE <553250@cart.ads>
2019-05-15 15:15:15 -0700

    made some changes to the css and the html and also added some images to the folder

--- contact.php
+++ contact.php
@@ -5,7 +5,7 @@
     <title>Contact</title>
         <link rel="stylesheet" href="css/index.css">
     <link href="https://fonts.googleapis.com/css?family=Fira+Sans" rel="stylesheet">
-
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
 </head>
 <body>
        <div class="top">
@@ -23,13 +23,33 @@
 <h1>CONTACT US</h1>
 <div class="flex-container">
     
-    <div class="contact">
+    <div class="fifty">
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus quo sunt dolores quisquam voluptatibus et nam, numquam beatae ipsam autem iusto, minus! Aliquam est illo ex vero, inventore facere! Enim!Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt veniam eaque nemo quaerat, ad placeat optio obcaecati autem maxime aliquam, nam expedita quis repellendus hic dicta quibusdam deleniti voluptatibus quas.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam eius repudiandae laborum, fuga, nobis asperiores odio similique ipsum provident ut porro, distinctio incidunt, voluptatibus sapiente modi quod recusandae? Quae, dolor.</p>
     </div>
-</div>
 <!-- add the icons of facebook, insta, and linkedin-->
+<div class="flex-container five"> 
 
-<button>Go follow me on</button>
+
+<div class="icon">
+     <i class="fab fa-facebook-square fa-3x  "></i>
+      <i class="fab fa-instagram fa-3x  "></i>
+
+       </div>  
+           
+               
+       
+<div class="ten">
+          <div class="icon">
+                <i class="fab fa-linkedin fa-3x  icon"></i>
+           <p>Follow us on</p>
+           </div>
+          
+            </div>
+            </div>
+</div>
+
+
+
 <div class="flex-container">
     <div class="box">
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quaerat nemo consequuntur vero soluta debitis sint veritatis dicta sunt praesentium totam rerum, quam, enim natus! Itaque repellat voluptates laudantium sit culpa?Lorem ipsum dolor sit amet, consectetur adipisicing elit. Necessitatibus, error. Ducimus quasi dolorum nemo iusto illum possimus corporis eveniet numquam ratione, quos assumenda impedit! Ex praesentium unde, sint deleniti ducimus!</p>
--- index.php
+++ index.php
@@ -88,8 +88,7 @@
          <div class="three">
             <i class="fas fa-user fa-7x" ></i>
                  <p class="left">I highly recommend A&M for all of your tax service needs.  Not only did they provide excellent customer service, but they also made sure not to miss a single thing with my return.  Their prices are very reasonable, they're very quick, and they take the time to get to build personal relationships with you.  Thank you A&M!!</p>
-                 <img src="/imgs/stars-temp.jpg" alt="">
-             <!--   <img src="http://placehold.it/200x50" alt="replace me"> -->
+                 <img src="./imgs/stars-temp.jpg" alt="">
          </div>
             
             <div class="three">
@@ -97,12 +96,14 @@
 <span style="color:#2222c9;"><i class="fas fa-user fa-7x"></i>
                     </span>                <p class="left">A&M provided amazing customer service while providing specific details of the process. This place is time efficient and affordable! I would definitely recommend to others. Staff is great and personable!
                  <p>
-                  <img src="http://placehold.it/150x50" alt="replace me">
+                                  <img src="./imgs/stars-temp.jpg" alt="">
+
          </div>
     <div class="three">
 <i class="fas fa-user fa-7x"></i>
         <p class="left">I have used larger tax companies until this year when I tried A&M. I cannot say enough about how customer sensitive they are. The customer service was amazing and it cost much less than I was paying before. They did not miss a thing and filing was done in a fraction of the time. Highly recommended. These guys are legit and I will be returning next year. Thank you guys for the great experience.</p>
-             <img src="http://placehold.it/150x50" alt="replace me">
+                             <img src="./imgs/stars-temp.jpg" alt="">
+
      </div>
     </div>
     
@@ -115,7 +116,8 @@
         Sounds funny, but they even change the temp in the room to make sure I was comfortable! 
         
          I highly recommend them</p>
-        <img src="http://placehold.it/150x50" alt="replace me">
+                          <img src="./imgs/stars-temp.jpg" alt="">
+
    </div>
     
     
@@ -123,7 +125,8 @@
     <div class="three">
          <i class="fas fa-user fa-7x"></i>
              <p class="left">I am amazed on how quickly and accurately A&M Tax Service prepared my taxes.  A line by line review was thoroughly and patiently explained.  With the ever changing tax laws, I trust their tax planning advice for the next year.  Reasonable price and excellent customer service warrants more than 5 stars.</p>
-           <img src="http://placehold.it/150x50" alt="replace me">
+                              <img src="./imgs/stars-temp.jpg" alt="">
+
      </div>
     
        
@@ -131,7 +134,8 @@
          <div class="three">
             <i class="fas fa-user fa-7x"></i>
                  <p class="left">John is extremely knowledgeable and helpful. He is always reliable and accommodating to his clients. He cuts out wasted time and paperwork that I’ve dealt with at other tax prep companies that overcharge for nice offices and “peace of mind”. He gets to the point and shows you your options. When I had audits from previous companies filings, he fixed the mess. Highly recommend</p>
-               <img src="http://placehold.it/150x50" alt="replace me">
+                                  <img src="./imgs/stars-temp.jpg" alt="">
+
          </div>
     </div>
 
@@ -139,14 +143,16 @@
    <div class="three">
         <i class="fas fa-user fa-7x"></i>
         <p class="left">John is very professional and makes your tax experience easy. He's very detailed and takes time explaining everything. We will definitely be back next year!!</p>
-          <img src="http://placehold.it/150x50" alt="replace me">
+                          <img src="./imgs/stars-temp.jpg" alt="">
+
     </div>
     
     
     <div class="three">
          <i class="fas fa-user fa-7x"></i>
              <p class="left">Very professional, timely, and efficient. I have been seeing John for taxes for two years now and it’s always been a seamless process. Highly recommend!</p>
-               <img src="http://placehold.it/150x50" alt="replace me">
+                               <img src="./imgs/stars-temp.jpg" alt="">
+
      </div>
     
         
@@ -154,7 +160,8 @@
             <i class="fas fa-user fa-7x"></i>
                  <p class="left">Fantastic service, great price! They get you what you are entitled to!
                  </p>
-               <img src="http://placehold.it/150x50" alt="replace me">
+                                  <img src="./imgs/stars-temp.jpg" alt="">
+
          </div>
 </div>
 
