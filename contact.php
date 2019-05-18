824bb55c34d15561c570002aa0c993bfa1c2ff15
Jasmine Barajas <45834526+Jasmineb0112@users.noreply.github.com>
2019-05-18 01:07:58 -0700

    updated the ph

--- contact.php
+++ contact.php
@@ -7,8 +7,9 @@
         <link rel="stylesheet" href="css/index.css">
     <link href="https://fonts.googleapis.com/css?family=Abel|Raleway" rel="stylesheet">
 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
+
 </head>
-<body>
+<body onload="initialize()">
        <div class="top">
           <img class="float" src="imgs/logo-2.jpg" alt="" width="128" height="76">
            <ul>
@@ -20,80 +21,68 @@
     </div>
         <!-- -->
 <!-- the image is for the appoinment and the team page, the h1 is going on top of the image -->
-     <img src="http://placehold.it/1890x400" alt="replace me">
-     <div class="map">
- <script async defer
-src="https://maps.googleapis.com/maps/api/js?key=    YOUR_API_KEY   &callback=initMap">
-</script>   
-</div>
-
 <h1>CONTACT US</h1>
-<div class="flex-container">
-    
-    <div class="fifty">
-        <p class="left">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus quo sunt dolores quisquam voluptatibus et nam, numquam beatae ipsam autem iusto, minus! Aliquam est illo ex vero, inventore facere! Enim!Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt veniam eaque nemo quaerat, ad placeat optio obcaecati autem maxime aliquam, nam expedita quis repellendus hic dicta quibusdam deleniti voluptatibus quas.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam eius repudiandae laborum, fuga, nobis asperiores odio similique ipsum provident ut porro, distinctio incidunt, voluptatibus sapiente modi quod recusandae? Quae, dolor.</p>
+       <div class="flex-container">
+        
+        <div class="fifty">
+            <p class="left">Thank you so much for visiting our website! We hope that you were able to make an appointment with one of our representatives. We’re excited to meet you and help you get the best out of your tax experience! Our social media are linked throughout the page so if you have any questions, feel free to contact us via social media or email. We thank you again for your time and hope to see you soon!
+</p>
+        </div>
+    <!-- add the icons of facebook, insta, and linkedin-->
+   
     </div>
-<!-- add the icons of facebook, insta, and linkedin-->
-<div class="flex-container five"> 
 
-
-<div class="icon">
-     <i class="fab fa-facebook-square fa-3x  "></i>
-      <i class="fab fa-instagram fa-3x  "></i>
-
-       </div>  
-           
-               
+<span class="half-text">
+        
        
-<div class="ten">
-          <div class="icon">
-                <i class="fab fa-linkedin fa-3x  icon"></i>
-           <p>Follow</p>
-           </div>
-          
-            </div>
-            </div>
-</div>
+    <!-- add the icons of facebook, insta, and linkedin-->
+    <div class="flex-container "> 
+    
+         <i class="fab fa-facebook-square fa-3x  "></i>
+          <i class="fab fa-instagram fa-3x  "></i>
+          <i class="fab fa-linkedin fa-3x  icon"></i>
+          <p>Follow us on</p>
+              
+                </div>
+    </span>
 
 
-
-<div class="flex-container">
-    <div class="box">
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quaerat nemo consequuntur vero soluta debitis sint veritatis dicta sunt praesentium totam rerum, quam, enim natus! Itaque repellat voluptates laudantium sit culpa?Lorem ipsum dolor sit amet, consectetur adipisicing elit. Necessitatibus, error. Ducimus quasi dolorum nemo iusto illum possimus corporis eveniet numquam ratione, quos assumenda impedit! Ex praesentium unde, sint deleniti ducimus!</p>
-    </div>
-</div>
 <div class="flex-foot">
 <div class="foot flex-container">
- <footer>
-<div class="ten"><i class="fab fa-facebook-square fa-3x"></i></div>
-<div class="ten"><i class="fab fa-instagram fa-3x"></i></div>
-<div class="ten"><i class="fab fa-linkedin fa-3x"></i></div> 
-
-<div class="fourty">
-    <h2>Pages</h2>
-    <ul>
-                <li><a href="index.php">Home</a></li>
-                 <br>
-                  <li><a href="team.php">Team</a></li>             <br>
+<footer class="foot">
+    <div class="flex-container">
+        <div class="five">
+               <div class="ten"><i class="fab fa-facebook-square fa-3x"></i></div>
+                <div class="ten"><i class="fab fa-instagram fa-3x"></i></div>
+                <div class="ten"><i class="fab fa-linkedin fa-3x"></i></div>
+        </div>
+        <div class="quarter">
+                <h2 class="font">Pages:</h2>
+                        <ul>
+                            <li><a class="font"  href="index.php">Home</a></li>
+                             <br>
+                          <li><a class="font" href="team.php">Team</a></li>             <br>
     
-               <li><a href="appointment.php">Appointments</a></li>             <br>
+                           <li><a class="font" href="appointment.php">Appointments</a></li>             <br>
     
-               <li><a href="contact.php">Contact</a></li>             <br>
-    
-               </ul>
-</div>
-<br>
-
-<div class="fourty">
-    <h3>Email:</h3>
-    <p>taxserviceam@gmail.com</p>
-    <h3>Phone Number:</h3>
-    <p>(559) 903-8550</p>
-    <h3>Address:</h3>
-    <p>473 N. Fresno St. 93701</p>
-    <p>Fresno, CA</p>
-    <img src="http://placehold.it/200x50" alt="replace me">
-</div>
+                           <li><a class="font" href="contact.php">Contact</a></li>             <br>
+                
+                           </ul>
+                        </div>
+            <div class="quarter">
+                <h3 class="font">Email:</h3>
+                    <p class="font">taxserviceam@gmail.com</p>
+                <h3 class="font">Phone Number:</h3>
+                    <p class="font">(559) 903-8550</p>
+               
+            </div>          
+            <div class="quarter">
+                 <h3 class="font">Address:</h3>
+                    <p class="font">473 N. Fresno St. 93701</p>
+                    <p class="font">Fresno, CA</p>
+                <img src="./imgs/logo-2.jpg" alt=""width="85.2" height="50.66">
+            </div>  
+    </div>
 </footer>
  </div>
     </div>
